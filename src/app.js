53198444966e2fff0dbc58ef601edfc16bcab5ea
MauRams <maurice.ramsbottom@gmail.com>
2020-03-28 12:32:59 +0000

    Added partials

--- src/app.js
+++ src/app.js
@@ -1,45 +1,51 @@
 //Set up packages
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 // this needs to be declared before the path.join
 const app = express()
-// provided in the wrapper function, we need to tell the app where to find the dir where the views are stored
-const publicDir = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
-console.log(viewsPath);
 
-// index.html page served using app.use
+// Define paths for express config
+const publicDir = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+//setup static dir to serve
 app.use(express.static(publicDir))
 
 /****************/
 /**** Routes ****/
 /****************/
 
-// added hbs view engine
-app.get('', (req, res) => {
-    console.log('rendering index template');
-    
+// root
+app.get('', (req, res) => {   
     res.render('index', {
         title: 'Weather App',
         name: 'Maurice'
     })
 })
-
+// about
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Page',
         name: 'Maurice'
     })
 })
-
+// help
 app.get('/help', (req, res) => {
     res.render('help', {
         doc_title: 'Help Page',
-        title: 'Help is on the way!'
+        helpText: 'This is some help text',        
+        title: 'Help is on the way!',
+        name: 'Maurice'
     })
 })
+
+
 /****************/
 /** Run Server **/
 /****************/
