4076b8f2f136f77d4dbfe12fb90ef6abb71bd88d
MauRams <maurice.ramsbottom@gmail.com>
2020-03-29 00:21:26 +0000

    Added client side call to API, currently prints to the console

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,24 @@
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            console.log(data.error);
-            
-        }else{
-            console.log(data.forecast);
-            console.log(data.location);
-        }
+const weatherForm = document.querySelector('form')
+
+const searchElement = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    //prevent the browser refresh
+    e.preventDefault()
+
+    const location = searchElement.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+
+            } else {
+                console.log(data.forecast);
+                console.log(data.location);
+            }
+        })
     })
+    
+    
 })
